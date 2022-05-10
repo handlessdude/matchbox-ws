6adae76713e4b117789cbf6a24884de2d4e7223a
Elis <handlessdude@gmail.com>
2022-05-10 20:22:17 +0300

    make messages output more alike usual chats

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ import {
     existsSync
 } from "fs";
 
+/**
+ * системный id
+ * */
+const SYSTEM = "SYSTEM";
 const clients = {};
 
-const log = existsSync('log') && readFileSync('log')
-const messages = JSON.parse(log) || [];
+const log = existsSync('log') && readFileSync('log', 'utf-8');
+const messages = log? JSON.parse(log) : [];
 
 const wss = new WebSocketServer({
     port: 8000
@@ -18,17 +22,31 @@ const wss = new WebSocketServer({
 wss.on("connection", ws => {
     const id = uuid();
     clients[id] = ws;
-    console.log(`New client ${id}`);
+
+    console.log(`New client ${id} appeared`);
+    Object.entries(clients).forEach(([clientId, clientWs]) => {
+        if (clientId!==id) {
+            clientWs.send(JSON.stringify([{
+                id: SYSTEM,
+                data: `New client ${id} appeared`
+            }]))
+        }
+    })
+
+    ws.send(JSON.stringify([{
+        id: SYSTEM,
+        data: `Your ID = ${id}`
+    }]))
+    /*отошлем также историю сообщений*/
     ws.send(JSON.stringify(messages))
 
     ws.on('message', (rawMessage) => {
         const data = JSON.parse(rawMessage);
-        console.log(`client ${id} | message = ${data}`)
+        console.log(`client ${id}: ${data}`)
         messages.push({id, data});
-        for (const clientId in clients) {
-            //сюда видимо пойдет типа "сколько осталось спичек"
-            clients[clientId].send(JSON.stringify({id, data}))
-        }
+        Object.values(clients).forEach(clientWs => {
+            clientWs.send(JSON.stringify([{id, data}]))
+        })
     })
 
     ws.on('close', () => {
